bb4c3d3f92e726effc6a8ad97846ffcfa63d6b52
Aran Fononi <aaronfononi@gmail.com>
2025-03-26 01:27:44 +0330

    Create Navigation Stack and List to show Datas

--- Wish List/ContentView.swift
+++ Wish List/ContentView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) private var modelContext
+    @Query private var wishes: [Wish]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(wishes) { wish in
+                    Text(wish.title)
+                }
+            }// List
+            .navigationTitle("Wishlist")
         }
-        .padding()
     }
 }
-
+    
 #Preview {
     ContentView()
+        .modelContainer(for: Wish.self, inMemory: true)
 }
