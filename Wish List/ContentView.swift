35f6892d81d3d5bb9b4b103881c3c1fe498b93dd
Aran Fononi <aaronfononi@gmail.com>
2025-03-26 01:47:17 +0330

    Add Sample Data

--- Wish List/ContentView.swift
+++ Wish List/ContentView.swift
@@ -30,7 +30,22 @@ struct ContentView: View {
     }
 }
     
-#Preview {
+
+#Preview("List with Sample Data") {
+    let container = try! ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    container.mainContext.insert(Wish(title: "Test Wish"))
+    container.mainContext.insert(Wish(title: "Buy a Mac"))
+    container.mainContext.insert(Wish(title: "Finish This Chapter"))
+    container.mainContext.insert(Wish(title: "Buy an iPhone"))
+    container.mainContext.insert(Wish(title: "Master SwiftData"))
+
+    return ContentView()
+        .modelContainer(container)
+    
+}
+
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Wish.self, inMemory: true)
 }
