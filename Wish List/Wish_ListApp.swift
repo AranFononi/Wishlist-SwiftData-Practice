ad22f96fe98b795bee7ab7103a008849df35104e
Aran Fononi <aaronfononi@gmail.com>
2025-03-26 01:03:47 +0330

    Initial Commit

---
+++ Wish List/Wish_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wish_ListApp.swift
+//  Wish List
+//
+//  Created by Aran Fononi on 26/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Wish_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
